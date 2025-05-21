4ad3768d40ea4c169327b4ff78797be8c2ebb55a
bbyg-0 <baba.yg.314@gmail.com>
2025-05-21 21:31:09 +0700

    X

--- commonFiles/clientLinux.c
+++ commonFiles/clientLinux.c
@@ -31,7 +31,7 @@ int main(int argc, char const* argv[]){
 	
 	// Convert IPv4 and IPv6 addresses from text to binary
 	// form
-	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+	if (inet_pton(AF_INET, "192.168.0.111", &serv_addr.sin_addr) <= 0) {
         	printf("\nInvalid address/ Address not supported \n");
 		return -1;
 	}
