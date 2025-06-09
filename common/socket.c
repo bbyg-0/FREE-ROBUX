caf91d79d45f45c8c8551822e9e7d0c0d5ab8085
bbyg-0 <hizba.shafwatuddin.tif424@polban.ac.id>
2025-06-09 14:22:02 +0700

    cross platform multi-threading socket ptogramming untuk server maupun client

--- common/socket.c
+++ common/socket.c
@@ -1,30 +1,43 @@
 #include "socket.h"
 #include "common.h"
 
-#include <pthread.h>
-#include <string.h>
-#include <errno.h>
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <fcntl.h>
+#ifdef _WIN32
+#else
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <pthread.h>
+#include <string.h>
+#include <arpa/inet.h>
+#endif
+
+#include <errno.h>
+#include <stdio.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 
 void inisialisasiParamThread(paramThread * param){
+#ifdef _WIN32
+	if((param) == NULL) return;
+
+	(param)->serverSocket = INVALID_SOCKET;
+	(param)->clientSocket = INVALID_SOCKET;
+	(param)->addrlen = -1;
+	(param)->litAddress = NULL;
+	(param)->socketStatus = '\0';
+#else
 	if(isEmpty(param)) return;
 	(param)->serverSocket = -1;
 	(param)->clientSocket = -1;
 	(param)->addrlen = 0;
 	(param)->litAddress = NULL;
 	(param)->socketStatus = '\0';
+#endif
 }
 
 void isiStatus(paramThread * param, char status){
 	if(isEmpty(param)) return;
-	if(status != 's' && status != 'c' && status != 'C'){
-		printf("KARAKAKTER YANG DITERIMA HANYA:\n\t- s : server\n\t- c : client\n");
+	if(status != 's' && status != 'c' && status != 'C' && status != 'S' && status != 'x' && status != 'X'){
 		return;
 	}else printf("ISI STATUS BERHASIL\n");
 
@@ -44,134 +57,211 @@ void isiAddress(paramThread * param, char * address){
 	(param)->litAddress = myStrdup(address);
 }
 
+// JENIS-JENIS STATUS BUAT CLIENT:
+// S : awalan, gak konek ke server
+// s : konek ke server
+// X : coba konek atau pernah konek tapi gak lagi karena server mati
+#ifdef _WIN32
+DWORD WINAPI serverSocket(LPVOID paramT){
+	paramThread * param = (paramThread *)paramT;
+
+	(param)->addrlen = sizeof((param)->address);
+
+	while(1){
+		(param)->serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+		if ((param)->serverSocket == INVALID_SOCKET) Sleep(500);
+		else break;
+	}
+
+	(param)->address.sin_family = AF_INET;
+	(param)->address.sin_addr.s_addr = INADDR_ANY;
+
+	while(1){
+		if (bind((param)->serverSocket,
+			(struct sockaddr *)&(param)->address,
+			sizeof((param)->address)) == SOCKET_ERROR) Sleep(500);
+		else break;
+	}
+
+	while(1){
+		if(listen((param)->serverSocket, 3) < 0) Sleep(500);
+		else break;
+	}
+
+	while(1){
+		while((param)->socketStatus == 'S' || (param)->socketStatus == 'X'){
+			printf("GETTING CLIENT:\n");
+			(param)->clientSocket = accept((param)->serverSocket,
+							(struct sockaddr *)&(param)->address,
+							&(param)->addrlen);
+			if ((param)->clientSocket == INVALID_SOCKET) Sleep(500);
+			else (param)->socketStatus = 's';
+		}
+		while((param)->socketStatus == 's') Sleep(500);
+	}
+}
+#else
 void * serverSocket(void * vParam){
-	int pass = -1;
 	int opt = 1;
 
 	paramThread * param = (paramThread *)vParam;
+
+	isiStatus(param, 'S');
 	
 	while(1){	
-		while(pass < 0){
-			usleep(500000);
+		while(1){
 			((param)->serverSocket) = socket(AF_INET, SOCK_STREAM, 0);
-			pass = ((param)->serverSocket);
+			if ((param)->serverSocket < 0) usleep(500000);
+			else break;
+
 		}
-		pass = -1;
-		// Forcefully attaching socket to the port 8080
-		while(pass < 0){
-			usleep(500000);
-			pass = setsockopt(((param)->serverSocket), SOL_SOCKET,
+		while(1){
+			if(setsockopt(((param)->serverSocket), SOL_SOCKET,
 					SO_REUSEADDR | SO_REUSEPORT, 
-					&opt, sizeof(opt));
+					&opt, sizeof(opt)) < 0) usleep(500000);
+			else break;
 		}
-		pass = -1;
 	
 		(param)->address.sin_family = AF_INET;
 		(param)->address.sin_addr.s_addr = INADDR_ANY;
 		
 		int flags = fcntl(((param)->serverSocket), F_GETFL, 0);         // Get current flags
 									
-		while(pass < 0){
-			usleep(500000);
-			pass = fcntl(((param)->serverSocket), F_SETFL, flags | O_NONBLOCK);
+		while(1){
+			if(fcntl(((param)->serverSocket), F_SETFL, flags | O_NONBLOCK) >= 0) break;
+			else usleep(500000);
 		}
-		pass = -1;
-		while(pass < 0){
-			usleep(500000);
-			pass = bind(((param)->serverSocket), (struct sockaddr*)&(param->address), sizeof((param)->address));
+
+		while(1){
+			if(bind(((param)->serverSocket),
+				(struct sockaddr*)&(param->address),
+				sizeof((param)->address)) < 0) usleep(500000);
+			else break;
 		}
-		pass = -1;
 	
-		while(pass < 0){
-			usleep(500000);
-			pass = listen(((param)->serverSocket), 3);
+		while(1){
+			if(listen(((param)->serverSocket), 3) < 0) usleep(500000);
+			else break;
 		}
 	
 		printf("SEBELUM LOOP\n");
 		while(1){
 			while(param->clientSocket < 0){
+				(param)->socketStatus = 'S';
 				if ((param->clientSocket = accept(((param)->serverSocket), 
 								(struct sockaddr*)&(param)->address, 
-								&(param)->addrlen)) < 0) {
-					if(errno == EAGAIN || errno == EWOULDBLOCK){
-						continue;
-					}
-				}else{
-					break;
-				}
+								&(param)->addrlen)) < 0)
+				usleep(500000);
+				else {(param)->socketStatus = 's'; break;}
 			}
-			usleep(100000);
+			usleep(1000000);
 		}
 	}
 
 	return NULL;
 }
+#endif
+// JENIS-JENIS STATUS BUAT CLIENT:
+// C : awalan, gak konek ke server
+// c : konek ke server
+// x : coba konek atau pernah konek tapi gak lagi karena server mati
+#ifdef _WIN32
+DWORD WINAPI clientSocket(LPVOID paraM){
+	paramThread * param = (paramThread *)paraM;
 
+	while(1){
+		while(1){
+		(param)->clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+			if ((param)->clientSocket == INVALID_SOCKET) Sleep(500);
+			else break;
+		}
+	
+		(param)->address.sin_family = AF_INET;
+		(param)->address.sin_addr.s_addr = inet_addr((param)->litAddress);
+	
+		while(1){
+			if (connect((param)->clientSocket,
+					(struct sockaddr *)&(param)->address,
+					sizeof((param)->address)) < 0) Sleep(500);
+			else break;
+		}
+	
+		printf("CONNECTED TO SERVER\n");
+
+		(param)->socketStatus = 'c';
+
+		while((param)->socketStatus == 'c') {Sleep(5000);}
+		printf("SEBELUM WHILE: %c\n", (param)->socketStatus);
+
+	
+		closesocket((param)->clientSocket);
+	}
+	return 0;
+}
+#else
 void * clientSocket(void * vParam){
 	int opt = 1;
-	int pass = -1;
 	paramThread * param = (paramThread *)vParam;
 
 	while(1){
 		isiStatus(param, 'C');
-		while(pass < 0){
-			usleep(500000);
-			((param)->clientSocket = socket(AF_INET, SOCK_STREAM, 0));	
-			pass = (param)->clientSocket;
+		while(1){
+			(param)->clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+			if((param)->clientSocket < 0) usleep(500000);
+			else break;
 		}
-		pass = -1;
 	
-		while(pass < 0){
-			usleep(500000);
-			pass = setsockopt(((param)->clientSocket), SOL_SOCKET,
+		while(1){
+			if(setsockopt(((param)->clientSocket), SOL_SOCKET,
 					SO_REUSEADDR | SO_REUSEPORT, 
-					&opt, sizeof(opt));
+					&opt, sizeof(opt)) < 0) usleep(500000);
+			else break;
 		}
-		pass = -1;
 	
 		(param)->address.sin_family = AF_INET;
+		(param)->address.sin_addr.s_addr = inet_addr((param)->litAddress);
+
 		
-		while(pass < 0){
-			usleep(500000);
-			pass = inet_pton(AF_INET, (param)->litAddress, &(param)->address.sin_addr);
+		while(1){
+			if(inet_pton(AF_INET, (param)->litAddress, &(param)->address.sin_addr) < 0 )
+				usleep(500000);
+			else break;
 		}
-		pass = 0;
 	
-		// JENIS-JENIS STATUS BUAT CLIENT:
-		// C : awalan, gak konek ke server
-		// c : konek ke server
-		// x : pernah konek ke server, tapi gak lagi karena server mati
-		while(pass == 0){
-			while ((param)->socketStatus == 'C' && pass == 0){
-				usleep(50000);
+		while(1){
+			if ((param)->socketStatus == 'C'){
 				if( connect((param)->clientSocket,
 						(struct sockaddr*)&(param)->address, 
 						sizeof((param)->address)) >= 0)
 					(param)->socketStatus = 'c';
-				else {
-					if ((param)->socketStatus == 'x'){
-						pass = -1;
-						close((param)->clientSocket);
-						break;
-					}
-				}
-				printf("GETTING SERVER\n");
+				else (param)->socketStatus = 'x';
 			}
-			if ((param)->socketStatus == 'x'){
-				pass = -1;
-				close((param)->clientSocket);
-				break;
-			}
-		usleep(1000000);
+			while((param)->socketStatus == 'c') usleep(500000);
+			if ((param)->socketStatus == 'x'){ close((param)->clientSocket); break;}
 		}
-	printf("KELUAR LOOP\n");
-	usleep(1000000);
+		printf("\nKELUAR LOOP\n"); usleep(1000000);
 	}
-
-
 	return NULL;
 }
+#endif
 
+#ifdef _WIN32
+DWORD WINAPI sendMessage(LPVOID paramT){
+	char buffer[100] = {0};
+
+	paramThread * param = (paramThread *)paramT;
+
+	while(1){
+		while((param)->socketStatus == 'c' || (param)->socketStatus == 's'){
+		printf("SEND:\t"); secureInputString(buffer, 1024);
+		send((param)->clientSocket, buffer, strlen(buffer), 0);
+		memset(buffer, 0, strlen(buffer));
+		}
+		Sleep(1000);
+	}
+	return 0;
+}
+#else
 void * sendMessage (void * c){
 	printf("INSIDE sendMessage\n");
 	int * client = (int *)(c);
@@ -185,7 +275,34 @@ void * sendMessage (void * c){
 	}
 	return NULL;
 }
+#endif
 
+#ifdef _WIN32
+DWORD WINAPI getMessage(LPVOID paramT){
+	char server_reply[100] = {0};
+
+	paramThread * param = (paramThread *)paramT;
+
+	int recv_size = 0;
+
+	while(1){
+		while((param)->socketStatus == 'c' || (param)->socketStatus == 's'){
+			recv_size = recv((param)->clientSocket, server_reply, sizeof(server_reply), 0);
+			if (recv_size == 0 || recv_size == SOCKET_ERROR) {
+				if((param)->socketStatus == 'c') (param)->socketStatus = 'x';
+				else (param)->socketStatus = 'X';
+				break;
+			} else {
+				server_reply[recv_size] = '\0';
+				printf("Server reply: %s\n", server_reply);
+				memset(server_reply, '\0', sizeof(server_reply));
+			}
+		}
+		Sleep(1000);
+	}
+	return 0;
+}
+#else
 void * getMessage (void * vParam){
 	printf("INSIDE getMessage\n");
 	char buffer[1024] = {0};
@@ -208,7 +325,10 @@ void * getMessage (void * vParam){
 				(param)->socketStatus = 'x';
 			}else return NULL;
 		}
-		}
+		}else printf("YAHAHA WAHYU\n");
+
+		usleep(1000000);
 	}
 	return NULL;
 }
+#endif
--- target/main.c
+++ target/main.c
@@ -1,37 +1,43 @@
+// File: client.c
+
 #ifdef _WIN32
 #include <winsock2.h>
-#pragma comment(lib, "ws2_32.lib")
 #else
-#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <pthread.h>
 #endif
 
 #include "../common/socket.h"
-
 #define PORT 8080
-int main(int argc, char const* argv[]){
-	
+#define ADDRESS "127.0.0.1"
+int main() {
 	paramThread param;
 
 	inisialisasiParamThread(&param);
 	isiPort(&param, PORT);
-	isiStatus(&param, 'S');
+	isiAddress(&param, ADDRESS);
+	isiStatus(&param, 'C');
+
 
 #ifdef _WIN32
-	HANDLE servSocket, sendMSG, getMSG;
-	DWORD servSocketId, sendMSGId, getMSGId;
+	HANDLE cliSocket, sendMSG, getMSG;
+	DWORD cliSocketId, sendMSGId, getMSGId;
 
 	WSADATA wsa;
 	WSAStartup(MAKEWORD(2,2), &wsa);
 
-	servSocket = CreateThread(NULL, 0, serverSocket, (LPVOID)&param, 0, &servSocketId);
+	cliSocket = CreateThread(NULL, 0, clientSocket, (LPVOID)&param, 0, &cliSocketId);
 	sendMSG = CreateThread(NULL, 0, sendMessage, (LPVOID)&param, 0, &sendMSGId);
 	getMSG = CreateThread(NULL, 0, getMessage, (LPVOID)&param, 0, &getMSGId);
 
 
-	WaitForSingleObject(servSocket, INFINITE);
-	CloseHandle(servSocket);
+	WaitForSingleObject(cliSocket, INFINITE);
+	CloseHandle(cliSocket);
 
 	WaitForSingleObject(sendMSG, INFINITE);
 	CloseHandle(sendMSG);
@@ -39,22 +45,20 @@ int main(int argc, char const* argv[]){
 	WaitForSingleObject(getMSG, INFINITE);
 	CloseHandle(getMSG);
 #else
-	pthread_t servSocket, send, get;
+	pthread_t cliSocket, send, get;
 
-	pthread_create(&servSocket, NULL, serverSocket, (void *)&param);
-
+	pthread_create(&cliSocket, NULL, clientSocket, (void *)&param);
 	pthread_create(&send, NULL, sendMessage, (void *)&(param.clientSocket));
 	pthread_create(&get, NULL, getMessage, (void *)&param);
 
-	pthread_join(servSocket, NULL);
+	pthread_join(cliSocket, NULL);
 	pthread_join(send, NULL);
 	pthread_join(get, NULL);
 
 	// closing the connected socket
-	close((param).clientSocket);
-  
-	// closing the listening socket
-	close((param).serverSocket);
+	close((param).clientSocket);	
 #endif
+
 	return 0;
 }
+
