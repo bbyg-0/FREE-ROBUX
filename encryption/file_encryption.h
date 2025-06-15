3dd52b3e6679737a51ccbf5ac9d97f14c3915a20
Zein023 <zeinarr66@gmail.com>
2025-06-15 23:53:00 +0700

    more module for encrypting and decrypting

--- encryption/file_encryption.h
+++ encryption/file_encryption.h
@@ -2,5 +2,8 @@
 #define FILE_ENCRYPTION_H
 
 int encrypt_file(const char *input_file, const char *output_file, const char *password);
+int encrypt_folder(const char *input_dir, const char *output_dir, const char *password);
+int decrypt_file(const char *input_file, const char *output_file, const char *password);
+int decrypt_folder(const char *input_dir, const char *output_dir, const char *password);
 
 #endif
